795e0c1d3b0bdcb8d2b9f46ba389d01b0e4d8646
Katsiaryna Fedarovich <kathefy215@gmail.com>
2018-12-22 18:25:51 +0100

    Components added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,91 @@
 import React, { Component } from "react";
 import "./App.css";
 
+class UserName extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+  }
+  changeValueName = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+  render() {
+    return (
+      <input
+        type="text"
+        placeholder="Full Name"
+        name="name"
+        required="required"
+        value={this.state.value}
+        onChange={this.changeValueName}
+      />
+    );
+  }
+}
+class Email extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+  }
+  changeValueEmail = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+  render() {
+    return (
+      <input
+        type="email"
+        placeholder="Email"
+        name="email"
+        required="required"
+        value={this.state.value}
+        onChange={this.changeValueEmail}
+      />
+    );
+  }
+}
+class Password extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+  }
+  changeValuePassword = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+  render() {
+    return (
+      <input
+        type="password"
+        placeholder="Password"
+        name="password"
+        required="required"
+        value={this.state.value}
+        onChange={this.changeValuePassword}
+      />
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="form">
         <form>
           <div className="loginForm">
-            <input
-              type="text"
-              placeholder="Full Name"
-              name="name"
-              required="required"
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              name="email"
-              required="required"
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              required="required"
-            />
+            <UserName />
+            <Email />
+            <Password />
           </div>
           <button type="submit">Create Account</button>
         </form>
