22e3f8541b182ad464cfdea562239458c29f0e4b
Katsiaryna Fedarovich <kathefy215@gmail.com>
2018-12-22 18:09:08 +0100

    Basic structure added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="form">
+        <form>
+          <div className="loginForm">
+            <input
+              type="text"
+              placeholder="Full Name"
+              name="name"
+              required="required"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              required="required"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              required="required"
+            />
+          </div>
+          <button type="submit">Create Account</button>
+        </form>
       </div>
     );
   }
