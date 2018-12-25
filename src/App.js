94a8ce40590cce0922fda14c28f1fea394f4bda4
Katsiaryna Fedarovich <kathefy215@gmail.com>
2018-12-25 12:43:39 +0100

    Input component added

--- src/App.js
+++ src/App.js
@@ -3,59 +3,45 @@ import { withFormik, Form, Field } from "formik";
 import "./App.css";
 import * as Yup from "yup";
 
+const InputLine = ({ errorsName, touched, name, title, type }) => (
+  <div className="container">
+    <Field
+      className={errorsName && touched ? "inputText errorInput" : "inputText"}
+      type={type}
+      name={name}
+    />
+    <span className="labelFloat">
+      {title}
+      {errorsName && touched && <span className="error">{errorsName}</span>}
+    </span>
+  </div>
+);
+
 const App = ({ errors, touched, isSubmitting }) => (
   <div className="form">
     <Form>
       <div className="loginForm">
-        <div className="container">
-          <Field
-            className={
-              errors.name && touched.name ? "inputText errorInput" : "inputText"
-            }
-            type="text"
-            name="name"
-          />
-          <span className="labelFloat">
-            Full Name
-            {errors.name && touched.name && (
-              <span className="error">{errors.name}</span>
-            )}
-          </span>
-        </div>
-        <div className="container">
-          <Field
-            className={
-              errors.email && touched.email
-                ? "inputText errorInput"
-                : "inputText"
-            }
-            type="email"
-            name="email"
-          />
-          <span className="labelFloat">
-            Email
-            {errors.email && touched.email && (
-              <span className="error">{errors.email}</span>
-            )}
-          </span>
-        </div>
-        <div className="container">
-          <Field
-            className={
-              errors.password && touched.password
-                ? "inputText inputTextLast errorInput"
-                : "inputText inputTextLast"
-            }
-            type="password"
-            name="password"
-          />
-          <span className="labelFloat">
-            Password
-            {errors.password && touched.password && (
-              <span className="error">{errors.password}</span>
-            )}
-          </span>
-        </div>
+        <InputLine
+          errorsName={errors.name}
+          touched={touched.name}
+          name="name"
+          type="text"
+          title="Full Name"
+        />
+        <InputLine
+          errorsName={errors.email}
+          touched={touched.email}
+          name="email"
+          type="email"
+          title="Email"
+        />
+        <InputLine
+          errorsName={errors.password}
+          touched={touched.password}
+          name="password"
+          type="password"
+          title="Password"
+        />
       </div>
       <button type="submit" disabled={isSubmitting}>
         Submit
